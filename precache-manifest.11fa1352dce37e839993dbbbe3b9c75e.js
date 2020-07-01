9f022b592657df15411b54980be6135e901afffc
RonKbS <stephen.bamwite@gmail.com>
2020-07-01 22:04:11 +0300

    Updates

---
+++ precache-manifest.11fa1352dce37e839993dbbbe3b9c75e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58644ad2c9f628ff4c28d492bfe26445",
+    "url": "/ASRT_GG/index.html"
+  },
+  {
+    "revision": "ee92cfb5a7f37f5db947",
+    "url": "/ASRT_GG/static/css/2.ba87ed55.chunk.css"
+  },
+  {
+    "revision": "dc21551ee8803569368c",
+    "url": "/ASRT_GG/static/css/main.a19fd6df.chunk.css"
+  },
+  {
+    "revision": "ee92cfb5a7f37f5db947",
+    "url": "/ASRT_GG/static/js/2.019dd8ad.chunk.js"
+  },
+  {
+    "revision": "bff0dea366aca0eb341932f9f5879dad",
+    "url": "/ASRT_GG/static/js/2.019dd8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc21551ee8803569368c",
+    "url": "/ASRT_GG/static/js/main.99d2fe9d.chunk.js"
+  },
+  {
+    "revision": "c5b3e9e546feb91da32a",
+    "url": "/ASRT_GG/static/js/runtime-main.cd17f889.js"
+  },
+  {
+    "revision": "ccb98897d3d994ab9b3604827026b070",
+    "url": "/ASRT_GG/static/media/geoLogo.ccb98897.png"
+  },
+  {
+    "revision": "efa2921636f27785032aec07502000ba",
+    "url": "/ASRT_GG/static/media/geogecko.efa29216.png"
+  }
+]);
