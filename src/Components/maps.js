fa46ddaeae542b72135b38ab2b0000535dd27651
RonKbS <stephen.bamwite@gmail.com>
2020-07-28 12:01:13 +0300

    add loader for initial map appearance

--- src/Components/maps.js
+++ src/Components/maps.js
@@ -7,6 +7,8 @@ import {
 } from "react-leaflet";
 import Control from "react-leaflet-control";
 import { connect } from "react-redux";
+
+import LoadingScreen from "./loadingScreen";
 import {
   updatePieChartData,
   updateChartView,
@@ -278,7 +280,7 @@ class UgMap extends Component {
 
           </Map>
       return map_state;
-    } else return "Failed to load the map";
+    } else return <LoadingScreen />;
   }
 }
 const mapStateToProps = (state) => {
