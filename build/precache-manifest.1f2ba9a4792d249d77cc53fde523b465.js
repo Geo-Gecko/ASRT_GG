212b7d2c1bc1596d622348ac242fc5500e0641c5
RonKbS <stephen.bamwite@gmail.com>
2020-07-29 07:58:10 +0300

    track build files

---
+++ build/precache-manifest.1f2ba9a4792d249d77cc53fde523b465.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884e14f7794571099b239ad23442e3ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e26e420ebe1720d09c9b",
+    "url": "/static/css/2.ba87ed55.chunk.css"
+  },
+  {
+    "revision": "3c41117ad9112f9d0b19",
+    "url": "/static/css/main.a5774a6c.chunk.css"
+  },
+  {
+    "revision": "e26e420ebe1720d09c9b",
+    "url": "/static/js/2.c37b727f.chunk.js"
+  },
+  {
+    "revision": "a2a6b692b3052b6f2e352a2ffbb2ae01",
+    "url": "/static/js/2.c37b727f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c41117ad9112f9d0b19",
+    "url": "/static/js/main.9a4517e8.chunk.js"
+  },
+  {
+    "revision": "057783a0c830871ef432",
+    "url": "/static/js/runtime-main.18f0b0e0.js"
+  },
+  {
+    "revision": "efa2921636f27785032aec07502000ba",
+    "url": "/static/media/geogecko.efa29216.png"
+  }
+]);
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from '../redux/reducers/reducersIndex';
-
-const initialState = {};
-
-const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
-export default store;
- 
