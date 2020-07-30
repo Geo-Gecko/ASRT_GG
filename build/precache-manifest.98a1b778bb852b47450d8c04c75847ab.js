8a9a80053ebd29b68a35de1ff7dc96e54df2ea50
RonKbS <stephen.bamwite@gmail.com>
2020-07-30 12:35:37 +0300

    add temporal variation charts to production

---
+++ build/precache-manifest.98a1b778bb852b47450d8c04c75847ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f675f64cd67a3b13fdb2bade2464d2e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57eea0c877d7fbbd0d22",
+    "url": "/static/css/2.ba87ed55.chunk.css"
+  },
+  {
+    "revision": "809f82afc63bc309af99",
+    "url": "/static/css/main.5f4dea66.chunk.css"
+  },
+  {
+    "revision": "57eea0c877d7fbbd0d22",
+    "url": "/static/js/2.bfe945b2.chunk.js"
+  },
+  {
+    "revision": "a2a6b692b3052b6f2e352a2ffbb2ae01",
+    "url": "/static/js/2.bfe945b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809f82afc63bc309af99",
+    "url": "/static/js/main.ddbe2171.chunk.js"
+  },
+  {
+    "revision": "057783a0c830871ef432",
+    "url": "/static/js/runtime-main.18f0b0e0.js"
+  },
+  {
+    "revision": "efa2921636f27785032aec07502000ba",
+    "url": "/static/media/geogecko.efa29216.png"
+  }
+]);
